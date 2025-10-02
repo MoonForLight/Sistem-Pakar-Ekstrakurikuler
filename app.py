9faad8d51784c61ae224de74888c64151f228455
kefza <baiqalfiaz@gmail.com>
2025-10-02 15:03:15 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/question')
+def question():
+    return render_template("question.html")
+
+@app.route('/result')
+def result():
+    # Di sini nanti bisa ditambahkan logika untuk menentukan hasil
+    # Untuk contoh sederhana kita tampilkan hasil statis
+    rekomendasi = "Ekstrakurikuler Paduan Suara 🎤"
+    return render_template("result.html", rekomendasi=rekomendasi)
+
+if __name__ == '__main__':
+    app.run(debug=True)
